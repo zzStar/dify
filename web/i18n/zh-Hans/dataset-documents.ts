a61424f513844b3e18a144f25b6a41adcc940e26
ZhangMingxing <generalxxx@163.com>
2025-02-15 15:36:15 +0800

    修改web项目dockerfile

--- web/i18n/zh-Hans/dataset-documents.ts
+++ web/i18n/zh-Hans/dataset-documents.ts
@@ -1,7 +1,7 @@
 const translation = {
   list: {
     title: '文档',
-    desc: '知识库的所有文件都在这里显示，整个知识库都可以链接到 Dify 引用或通过 Chat 插件进行索引。',
+    desc: '知识库的所有文件都在这里显示，整个知识库都可以链接到应用引用。',
     learnMore: '了解更多',
     addFile: '添加文件',
     addPages: '添加页面',
@@ -51,7 +51,7 @@ const translation = {
     empty: {
       title: '还没有文档',
       upload: {
-        tip: '您可以上传文件，从网站同步，或者从网络应用程序（如概念、GitHub 等）同步。',
+        tip: '您可以上传文件',
       },
       sync: {
         tip: 'Dify 会定期从您的 Notion 中下载文件并完成处理。',
